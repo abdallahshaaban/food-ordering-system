32a15cf75520625570e056a668f9954029fe4ff9
abdalla shaaban <abdallah.shaaban@cis.asu.edu>
2023-09-24 12:32:18 +0300

    continue working along with MS course

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
@@ -52,7 +52,7 @@ public class OrderCreateCommandHandler {
         OrderCreatedEvent orderCreatedEvent = orderDomainService.validateAndInitiateOrder(order, restaurant);
         Order orderResult = saveOrder(order);
         log.info("Order is created with id: {}",orderResult.getId().getValue());
-        return orderDataMapper.orderToCreateOrderResponse(orderResult)
+        return orderDataMapper.orderToCreateOrderResponse(orderResult);
 
     }
 
